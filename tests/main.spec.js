7484f2226d4d15cd2c56e535cf14272c887efcf5
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-08-20 11:27:33 -0300

    ✅ add generic tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,17 @@
-import { expect } from 'chai';
+/* eslint-disable no-unused-vars */
+import Chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
 
 import {
   search, searchAlbums, searchArtists, searchTracks, searchPlaylists,
 } from '../src/main';
+
+Chai.use(sinonChai);
+sinonStubPromise(sinon);
+
+global.fetch = require('node-fetch');
 
 describe('Spotify Wrapper', function () {
   describe('Smoke Tests', function () {
@@ -24,6 +33,38 @@ describe('Spotify Wrapper', function () {
 
     it('Should exist the searchPlaylists method', function () {
       expect(searchPlaylists).to.exist;
+    });
+  });
+
+  describe('Generic Search', function () {
+    let fetchedStub;
+
+    beforeEach(function () {
+      fetchedStub = sinon.stub(global, 'fetch');
+    });
+
+    afterEach(function () {
+      fetchedStub.restore();
+    });
+
+    it('Should call fetch function', function () {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOn;
+    });
+
+    it('Should call fetch with the current URL', function () {
+      context('Passing one type', function () {
+        const artists = search('Incubus', 'artist');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+        const albums = search('Incubus', 'album');
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      });
+
+      context('Passing more than one type', function () {
+        const artistAndAlbum = search('Incubus', ['artist', 'album']);
+        expect(fetchedStub).to.have.been.calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+      });
     });
   });
 });
