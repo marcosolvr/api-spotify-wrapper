dbfa023be448b6c1c9b020a2ae380a2c96649cd7
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-11-11 14:50:59 -0300

    refactor: create utils.js

--- src/album.js
+++ src/album.js
@@ -1,12 +1,13 @@
+/* global fetch */
+
 import API_URL from './config';
+import toJSON from './utils';
 
 const TOKEN = 'BQCxAjLeD8NAV_S4c5qA9mvEIa0QF753PgnzSa9qZ4ca1Er-eFZdt2iBc3yk9orAuSbTo20r0rIPJFsoNXYndxewWmA3fFP59BRRtNl8ev1wcl0oiOXxd-R_qWknT622AcsZiZ7seRRXBfj4Lp103B-r8dQx';
 const HEADERS = {
   headers: { Authorization: `Bearer ${TOKEN}` },
 };
-// eslint-disable-next-line no-undef
-export const getAlbum = (query) => fetch(`${API_URL}/albums/?ids=${query}`, HEADERS).then((data) => data.json);
-// eslint-disable-next-line no-undef
-export const getAlbums = (query) => fetch(`${API_URL}/albums/?ids=${query}`, HEADERS).then((data) => data.json);
-// eslint-disable-next-line no-undef
-export const getAlbumTracks = (query) => fetch(`${API_URL}/albums/${query}/tracks`).then((data) => data.json);
+
+export const getAlbum = (query) => fetch(`${API_URL}/albums/?ids=${query}`, HEADERS).then(toJSON);
+export const getAlbums = (query) => fetch(`${API_URL}/albums/?ids=${query}`, HEADERS).then(toJSON);
+export const getAlbumTracks = (query) => fetch(`${API_URL}/albums/${query}/tracks`).then(toJSON);
--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
+/* global fetch */
+
 import API_URL from './config';
+import toJSON from './utils';
 
 export const search = (query, type) => {
-  // eslint-disable-next-line no-undef
   fetch(`${API_URL}/search?q=${query}&type=${type}`)
-    .then((data) => data.json());
+    .then(toJSON);
 };
 export const searchArtists = (query) => {
   search(query, 'artist');
