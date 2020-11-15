773d61ad534d96951547d51be412e3ade75f331a
Marcos Oliveira <marcos.abreu.oliveira@gmail.com>
2020-11-15 15:22:25 -0300

    fix function return to search.js

--- src/search.js
+++ src/search.js
@@ -8,19 +8,19 @@ const HEADERS = {
   headers: { Authorization: `Bearer ${TOKEN}` },
 };
 
-export const search = (query, type) => {
+export const search = (query, type) => (
   fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS)
-    .then(toJSON);
-};
-export const searchArtists = (query) => {
-  search(query, 'artist');
-};
-export const searchAlbums = (query) => {
-  search(query, 'album');
-};
-export const searchTracks = (query) => {
-  search(query, 'track');
-};
-export const searchPlaylists = (query) => {
-  search(query, 'playlist');
-};
+    .then(toJSON)
+);
+export const searchArtists = (query) => (
+  search(query, 'artist')
+);
+export const searchAlbums = (query) => (
+  search(query, 'album')
+);
+export const searchTracks = (query) => (
+  search(query, 'track')
+);
+export const searchPlaylists = (query) => (
+  search(query, 'playlist')
+);
