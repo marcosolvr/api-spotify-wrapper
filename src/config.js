ac5e80da5390175881091da4c59de851159a0803
Marcos Oliveira <marcos.abreu.oliveira@gmail.com>
2020-11-16 10:45:54 -0300

    refactor headers api spotify to config.js

--- src/config.js
+++ src/config.js
@@ -1,1 +1,6 @@
-export default 'https://api.spotify.com/v1';
+const TOKEN = 'BQBlP1U_7WWy7PXdqrnoihuM0ERx7x_5A-IlaKv_RVMqhwoi7Sm_gj135bAqpe-QcjWlXpc5D8bQKIF0HuLlL505j8THzLIGOqOmIO_Oorg3g2T1y9fe95RNpq6WS8X-Ljkt1dI3G-B7c9zwJvAo86pgxeWb';
+
+export const API_URL = 'https://api.spotify.com/v1';
+export const HEADERS = {
+  headers: { Authorization: `Bearer ${TOKEN}` },
+};
--- src/search.js
+++ src/search.js
@@ -1,12 +1,7 @@
 /* global fetch */
 
-import API_URL from './config';
+import { API_URL, HEADERS } from './config';
 import toJSON from './utils';
-
-const TOKEN = 'BQDARYEjIe_XhxANGT3N_Qc_PqMjeKsSx2vaWuQf-F6UL_eB1qKQS1o8E1lmsZJYsxY0m6UfnPYrFOZLf85JZXMisiC35vsdqqpcYUj3fYBpfgjy0brB9jhQu32U6k_R4gm6Pfe_MQLhbSB_3VlmIPYS-9hz';
-const HEADERS = {
-  headers: { Authorization: `Bearer ${TOKEN}` },
-};
 
 export const search = (query, type) => (
   fetch(`${API_URL}/search?q=${query}&type=${type}`, HEADERS)
